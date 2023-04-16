c9f52a604af5fd92964137bc1dbaa12639e902af
Com6235 <130489282+Com6235@users.noreply.github.com>
2023-04-16 09:48:30 +0300

    cmmit

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
   });
   // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-  let value = params.h2; // "some_value"
+  let value = params.easteregg; // "some_value"
 var hka = document.getElementById('param1-id')
-hka.innerHTML = value
+if (value == "true") {
+  hka.innerText = "You found an easter egg #1!"
+}
+
+function button() {
+  if (value == "true") {
+    var audio = new Audio("Media/oof.mp3")
+    audio.play()
+  }
+}
